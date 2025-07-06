3f19e567effdd09bce6b50bb5a101ddf6156c92d
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2025-07-06 10:10:58 +0200

    Migrate Hugo blog to CI/CD: build on GitHub Actions, ignore docs/, add dev config, update README

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: HUGO_ENV=development hugo server -D
 assets: cd themes/ferruccio/src && npm run dev 
+admin: cd gitcm && bun run dev
--- docs/dist/js/main.CCufmEYV.js
+++
@@ -1,1 +0,0 @@
-function e(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}console.log("Ferruccio Theme loaded");console.log("Dev changes reloaded");export{e as __vite_legacy_guard};
